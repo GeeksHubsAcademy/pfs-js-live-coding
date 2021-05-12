653bf773bd0a3fc98043ff35bb810c8c5192547a
Jupegarnica <juan@geekshubs.com>
2021-05-12 13:24:30 +0200

    tabla de multiplicar

--- ejercicios/tablaDeMultiplicar.js
+++ ejercicios/tablaDeMultiplicar.js
@@ -13,3 +13,27 @@ tablaDeMultiplicarConWhile(2);
 // for
 // for of
 // for in
+function tablaDeMultiplicarForIn(n) {
+  console.log('tablaDeMultiplicarForIn');
+
+  // var objeto = {
+  //   1: true,
+  //   2: true,
+  //   3: true,
+  //   4: true,
+  //   5: true,
+  //   6: true,
+  //   7: true,
+  //   8: true,
+  //   9: true,
+  //   10: true,
+  // };
+  var objeto = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (var key in objeto) {
+    // var index = key;
+    var index = objecto[key];
+
+    console.log(n, '*', index, '=', n * index);
+  }
+}
+tablaDeMultiplicarForIn(2);
