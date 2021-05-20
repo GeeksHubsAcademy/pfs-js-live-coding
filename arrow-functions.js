0676b504955b71aa8cc2322626bb15a9eccaa15b
Jupegarnica <juan@geekshubs.com>
2021-05-20 10:47:12 +0200

    this

--- arrow-functions.js
+++ arrow-functions.js
@@ -24,3 +24,55 @@ const createFilm3 = (title) => ({ title: title });
 const numbers = [1, 4, 2, 3, 99];
 numbers.sort((a, b) => a - b);
 console.log(numbers);
+
+(a) => a * 2;
+
+(a) => a * 2;
+(persona) => persona.name;
+(persona) => persona.name;
+({ name }) => name;
+
+const createPerson = (name, age) => ({ name: name, age });
+
+// this
+//////
+
+// function logBrand() {
+//   console.log(this.brand);
+// }
+
+// const logBrand = () => {
+//   console.log(this.brand);
+// };
+
+class Car {
+  constructor(brand) {
+    this.brand = brand;
+  }
+  brand = 'none';
+  sayBrand = () => {
+    console.log(this.brand);
+  };
+
+  runExternal(fn) {
+    fn();
+  }
+}
+
+class Bike {
+  constructor(brand) {
+    this.brand = brand;
+  }
+  brand = 'none';
+  // sayBrand = () => {
+  //   console.log(this.brand);
+  // };
+  sayBrand() {
+    console.log(this.brand);
+  }
+}
+
+const honda = new Car('honda');
+const orbea = new Bike('Orbea');
+
+honda.runExternal(orbea.sayBrand.bind(orbea));
